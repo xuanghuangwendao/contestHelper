b66a2d15a95085800f0471ca52339581be22eb23
bude <3406401355@qq.com>
2021-04-17 00:30:14 +0800

    去除无用模块

--- recommend-engine/src/main/java/com/bude/recommendengine/repository/ContestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bude.recommendengine.repository;
-
-import com.bude.recommendengine.entity.ContestEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ContestRepository extends CrudRepository<ContestEntity, Integer> {
-}
