65a39ba47d96b55aaf9ee12b35c6e05bca2fa67b
bude <3406401355@qq.com>
2021-04-17 19:01:00 +0800

    fix bug

--- user-server/src/main/java/com/bude/userserver/repository/MessageRepository.java
+++ user-server/src/main/java/com/bude/userserver/repository/MessageRepository.java
@@ -1,7 +1,6 @@
 package com.bude.userserver.repository;
 
 import com.bude.userserver.entity.MessageEntity;
-import com.bude.utils.model.Message;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
